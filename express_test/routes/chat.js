dab392e5c39927de3ee68c616b599239eb6cb923
Pablo Rivero <pablo.rivero1@usp.ceu.es>
2023-12-12 14:58:05 +0100

    fixed

--- express_test/routes/chat.js
+++ express_test/routes/chat.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-<<<<<<< HEAD
 router.get('/', function(req, res) {
   res.render('chat', {user: req.session.user, title: 'Chat'});
-=======
-router.get('/', function(req, res, next) {
-  res.render('chat', { title: 'Chat' });
->>>>>>> 772df81b72b8c76b18dc47e7df9d3079b2a8b7f7
 });
 
 module.exports = router;
